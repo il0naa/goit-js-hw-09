22b213ef9c2436540a2d4c6b189cd55923a5a6cc
il0naa <136170246+il0naa@users.noreply.github.com>
2023-11-12 15:18:50 +0100

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+let intervalId;
 
+    function getRandomHexColor() {
+      return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+    }
+
+    function startColorChange() {
+      if (!intervalId) {
+        document.querySelector('[data-start]').disabled = true;
+        intervalId = setInterval(() => {
+          document.body.style.backgroundColor = getRandomHexColor();
+        }, 1000);
+      }
+    }
+
+    function stopColorChange() {
+      clearInterval(intervalId);
+      document.querySelector('[data-start]').disabled = false;
+      intervalId = null;
+    }
+
+    document.querySelector('[data-start]').addEventListener('click', startColorChange);
+    document.querySelector('[data-stop]').addEventListener('click', stopColorChange);
