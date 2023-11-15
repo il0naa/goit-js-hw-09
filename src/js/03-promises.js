dbb4c0a6f561821d6b93cb01fb4347063afad29b
il0naa <136170246+il0naa@users.noreply.github.com>
2023-11-15 10:56:20 +0100

    fixed task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,46 +1,48 @@
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', submitForm);
+
+function submitForm(evt) {
+  evt.preventDefault();
+  const objData = {};
+  const formData = new FormData(form);
+  formData.forEach((value, key) => {
+    objData[key] = value;
+  });
+  createFunctions(objData);
+}
+
+function createFunctions(objData) {
+  for (let index = 0; index < objData.amount; index++) {
+    const position = index + 1;
+    const delay = Number(objData.delay) + Number(objData.step) * index;
+    const promise = createPromise(position, delay);
+
+    promise
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+}
+
 function createPromise(position, delay) {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve, rejact) => {
+    const shouldResolve = Math.random() > 0.3;
+    const obj = {
+      position,
+      delay,
+    };
     setTimeout(() => {
-      const shouldResolve = Math.random() > 0.3;
       if (shouldResolve) {
-        resolve({ position, delay, status: 'resolved' });
+        resolve(obj);
       } else {
-        reject({ position, delay, status: 'rejected' });
+        rejact(obj);
       }
     }, delay);
   });
-}
+} 
 
-document.querySelector('.form').addEventListener('submit', function (event) {
-  event.preventDefault();
-
-  const form = document.querySelector('.form');
-  const delayInput = form.querySelector('[name="delay"]');
-  const stepInput = form.querySelector('[name="step"]');
-  const amountInput = form.querySelector('[name="amount"]');
-
-  const initialDelay = parseInt(delayInput.value);
-  const step = parseInt(stepInput.value);
-  const amount = parseInt(amountInput.value);
-
-  function processPromise(index) {
-    if (index > amount) {
-      return;
-    }
-
-    const delay = initialDelay + (index - 1) * step;
-
-    createPromise(index, delay)
-      .then(result => {
-        console.log(`✅ Проміс ${result.position} виконано за ${result.delay}мс`);
-        processPromise(index + 1);
-      })
-      .catch(error => {
-        console.log(`❌ Проміс ${error.position} відхилено за ${error.delay}мс`);
-        processPromise(index + 1);
-      });
-  }
-
-  processPromise(1);
-});
 
